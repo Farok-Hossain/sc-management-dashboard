5409558f80954ea687f0f6a56248132d304e046a
Farok Hossain <farokafs7@gmail.com>
2024-09-16 23:25:02 +0600

    again updated

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/shared/Navbar";
+import Dashboard from "../Layout/Dashboard";
 
 const Main = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Dashboard></Dashboard>
       <Outlet></Outlet>
     </div>
   );
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-import Dashboard from "../Layout/Dashboard";
+// import Dashboard from "../Layout/Dashboard";
 import DashboardPage from "../Pages/Dashboard/Dashboard/DashboardPage";
 
 export const router = createBrowserRouter([
@@ -8,10 +8,10 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Main></Main>,
     children: [
-      {
-        path: "/",
-        element: <Dashboard></Dashboard>,
-      },
+      // {
+      //   path: "/",
+      //   element: <Dashboard></Dashboard>,
+      // },
       {
         path: "/dashboard",
         element: <DashboardPage></DashboardPage>,
